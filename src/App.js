8053132c08a81bffbfe9ef5fa178b5fb8f7080ca
progriro <progriro@gmail.com>
2020-07-10 00:28:45 +0900

    :rocket: create CounterHook.js :rocket:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+import CounterHook from "./components/CounterHook";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <CounterHook />
     </div>
   );
 }
